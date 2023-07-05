00aa5f555435de46ffa83a5d144c3f90d6f94de6
Madhu Hanumanthappa <129966659+madhuhanumanthappa@users.noreply.github.com>
2023-07-05 16:21:58 +0530

    Add files via upload
    New method transferAmountBetweenAccounts () is added in this AccountsService class.

---
+++ AccountsService.java
@@ -0,0 +1,36 @@
+package com.dws.challenge.service;
+
+import com.dws.challenge.domain.Account;
+import com.dws.challenge.repository.AccountsRepository;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class AccountsService {
+
+  @Getter
+  private final AccountsRepository accountsRepository;
+
+  @Autowired
+  public AccountsService(AccountsRepository accountsRepository) {
+    this.accountsRepository = accountsRepository;
+  }
+
+  public void createAccount(Account account) {
+    this.accountsRepository.createAccount(account);
+  }
+
+  public Account getAccount(String accountId) {
+    return this.accountsRepository.getAccount(accountId);
+  }
+
+  public ConcurrentHashMap<Account, Account> transferAmountBetweenAccounts(ConcurrentHashMap<Account, Account> accountDetails, BigDecimal transferAmountValue) {
+    return this.accountsRepository.transferAmountBetweenAccounts(accountDetails, transferAmountValue);
+  }
+}
+
+
